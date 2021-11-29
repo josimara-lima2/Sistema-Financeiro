594cffda72a4f056f0a36e79436115529c42a708
Josimara <josybehlul@gmail.com>
2021-11-29 17:35:31 -0300

    add personalizaçao de acordo com os valores informados

--- src/components/InputArea/index.tsx
+++ src/components/InputArea/index.tsx
@@ -8,39 +8,43 @@ type Props ={
 }
 export const InputArea = ({onAdd}:Props) =>{
     const [title,setTitle] = useState('')
-    const [value,SetValue] = useState<number>(0)
+    const [valueInput,SetValueInput] = useState<number>(0)
     const [dateAdd,setDateAdd] = useState('')
-    const [index,setIndex] = useState(0)
+    const [index,setIndex] = useState('rent')
 
     
     
 
     const handleAddEvent = () => {
+        if(title === ''){
+            alert('Título inválido')
+        }
+        if(valueInput === 0){
+            alert('Valor invalido')
+        }
+        
         let newItem:Item = {
             date: dateInput(dateAdd),
-            
-            category: 'food',
+            category:index,
             title: title,
-            value:value
+            value:valueInput
         }
         onAdd(newItem)
     }
-    
-
     
 
     return(
         <C.Container>
             <input type="date" onChange={e => setDateAdd(e.target.value)}/>
             
-            <select name="" id="" onChange={e => console.log(e.currentTarget)}>
+            <select name="op" id="" onChange={e => setIndex(e.target.value)}>
             
-                <option value="food" >Aluguel</option>
-                <option value="rent">Alimentação</option>
+                <option value="rent" >Aluguel</option>
+                <option value="food">Alimentação</option>
                 <option value="salary">Salario</option>
             </select>
             <input type="text" onChange={e => setTitle(e.target.value)}/>
-            <input type="number" onChange={e => SetValue(Number(e.target.value))} />
+            <input type="number" onChange={e => SetValueInput(Number(e.target.value))} />
            <button onClick={handleAddEvent}>Adicionar</button>
         </C.Container>
     )
